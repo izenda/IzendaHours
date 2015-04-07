8cc31c56dd7d70fa474bf94452e39f8a1f0caebb
Shahein <shahein.moussavi@izenda.com>
2015-04-07 09:59:19 -0400

    Cosmetic changes

--- IzendaHours/Controllers/AccountController.cs
+++ IzendaHours/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace IzendaHours.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Welcome", "Records");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -164,7 +164,7 @@ namespace IzendaHours.Controllers
                      //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                      //return View("DisplayEmail");
-                    return RedirectToAction("Index", "Records");
+                    return RedirectToAction("Welcome", "Records");
                 }
                 AddErrors(result);
             }
