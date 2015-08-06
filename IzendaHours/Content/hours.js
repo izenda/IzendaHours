251d35d66f927ba38169d06e6e8e43e1c9f83ba1
Shahein <shahein.moussavi@izenda.com>
2015-08-06 15:31:17 -0400

    Added Select2 dropdowns and restructured scripts

---
+++ IzendaHours/Content/hours.js
@@ -0,0 +1,78 @@
+﻿$("#timeTable").each(function () {
+    $(this).dataTable({
+        "aaSorting": [[0, 'desc']],
+        "stateSave": true,
+        "stateDuration": -1,
+        "footerCallback": function (row, data, start, end, display) {
+            var api = this.api(), data;
+
+            // Remove the formatting to get integer data for summation
+            var intVal = function (i) {
+               return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '') * 1 :
+                    typeof i === 'number' ?
+                    i : 0;
+            };
+
+            // Total over all pages
+            total = api
+                .column(4)
+                .data()
+                .reduce(function (a, b) {
+                    return intVal(a) + intVal(b);
+                });
+
+            // Total over this page
+            pageTotal = api
+               .column(4, { page: 'current' })
+               .data()
+               .reduce(function (a, b) {
+                   return intVal(a) + intVal(b);
+                }, 0);
+
+            // Update footer
+            $(api.column(4).footer()).html(
+               pageTotal + ' hours'
+            );
+        }
+    })
+    .columnFilter({
+       //sPlaceHolder: "head:before",
+       "bLengthChange": true,
+       "sDom": "lrtip",
+       "bAutoWidth": true, "sScrollY": "400px", "bPaginate": true, "bUseColVis": true, "bScrollCollapse": true, aoColumns: [{
+           type: "date-range"
+                 
+        },
+                   {
+                       type: "select"
+                              
+                   },
+                   {
+                       type: "select"
+
+                   }
+        ]
+
+    });
+
+});
+
+$(document).ready(function () {
+   $('#projectTable').DataTable();
+});
+
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax() {
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight - scrolled) + 'px');
+}
+
+$(window).scroll(function (e) {
+    parallax();
+});
+
+$(document).ready(function () {
+    $("#ProjectId").select2();
+    $("#TaskId").select2();
+});
